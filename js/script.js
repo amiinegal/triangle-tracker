2665ed5056a3b8be9b3055e502a0e9402b422ac9
amiinegal <you@example.com>
2019-02-17 14:27:03 +0300

    add js/

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function triangle() {
+ var sides = [];
+ sides.push(document.getElementById("side1").value);
+ sides.push(document.getElementById("side2").value);
+ sides.push(document.getElementById("side3").value);
+
+ var [a,b,c] = sides;
+ if(a.length === 0 || b.length === 0 || c.length === 0 ) {
+ } else {
+   side(a,b,c);
+ }
+}
+function side(a,b,c) {
+if(a === b && b === c && c === a) {
+alert("equilateral triangle")
+}
+else if (a + b > c && b + c > a && c + a > b) {
+alert("scalene triangle")
+}
+else if(a === b || b === c || c === a) {
+alert("isosceles triangle")
+}
+else {
+alert("not a triangle")
+}
+
+}
+
+ function reset() {
+   location.reload();
+}
